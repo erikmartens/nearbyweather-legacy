346071b36c67ae188534be811c0a7a8b459f958d
erikmartens <erikmartens@me.com>
2020-04-11 23:24:08 +0200

    added function to report non fatal errors

--- NearbyWeather/Commons/GlobalFunctions.swift
+++ NearbyWeather/Commons/GlobalFunctions.swift
@@ -7,10 +7,24 @@
 //
 
 import Foundation
+import Firebase
 
 func printDebugMessage(domain: String, message: String) {
   guard !BuildEnvironment.isReleaseEvironment() else {
     return
   }
   debugPrint("💥 \(domain) : \(message)")
+}
+
+func reportNonFatalError(_ error: NSError) {
+  Crashlytics.crashlytics().record(error: error)
+}
+
+func reportCustomNonFatalError(for domain: String, message: String) {
+  let error = NSError(
+    domain: domain,
+    code: -1,
+    userInfo: ["message": message]
+  )
+  Crashlytics.crashlytics().record(error: error)
 }

15db723f9e2bea5b492c6be69e32499e994e5477
erikmartens <erikmartens@me.com>
2020-04-19 22:43:06 +0200

    fix WeatherLocationManagementTableViewController inset

--- NearbyWeather/Scenes/Bookmarks Management Scene/WeatherLocationManagementTableViewController.swift
+++ NearbyWeather/Scenes/Bookmarks Management Scene/WeatherLocationManagementTableViewController.swift
@@ -24,24 +24,27 @@ final class WeatherLocationManagementTableViewController: UITableViewController,
   
   // MARK: - ViewController LifeCycle
   
+  override init(style: UITableView.Style) {
+    super.init(style: style)
+    
+    tableView.register(UINib(nibName: R.nib.singleLabelCell.name, bundle: R.nib.singleLabelCell.bundle),
+    forCellReuseIdentifier: R.reuseIdentifier.singleLabelCell.identifier)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     title = R.string.localizable.manage_locations()
-    
-    tableView.register(UINib(nibName: R.nib.singleLabelCell.name, bundle: R.nib.singleLabelCell.bundle),
-                       forCellReuseIdentifier: R.reuseIdentifier.singleLabelCell.identifier)
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    
     tableView.isEditing = true
   }
   
   // MARK: - TableViewDelegate
   
   override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
+    1
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {

bcc93e89e55c14d5319213813757558bcc197c57
erikmartens <erikmartens@me.com>
2020-04-20 22:47:22 +0200

    fixed cell selection action

--- NearbyWeather/Scenes/About App Scene/AboutAppTableViewController.swift
+++ NearbyWeather/Scenes/About App Scene/AboutAppTableViewController.swift
@@ -79,7 +79,7 @@ final class AboutAppTableViewController: UITableViewController, Stepper {
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
     
-    if indexPath.section == 0 && indexPath.row == 0 {
+    if indexPath.section == 1 && indexPath.row == 0 {
       UIApplication.shared.open(Constants.Urls.kAppStoreRatingDeepLinkUrl, completionHandler: nil)
       return
     }
